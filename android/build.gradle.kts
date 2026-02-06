0f632fded54d42485d261be3a7be070456727236
JinDannyDavid <71433164@continental.edu.pe>
2026-02-06 01:50:12 -0500

    fase alpha

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,9 +30,9 @@ buildscript {
     }
     
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.3.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10"
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         // Agregar esta línea:
-        classpath 'com.google.gms:google-services:4.3.15'
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
